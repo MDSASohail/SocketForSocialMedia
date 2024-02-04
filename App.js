0f65dfb91e73412c742c6902e153a65b544e74b5
Sohail <thisemailitakenbysohail@gmail.com>
2024-02-04 14:17:12 +0530

    Chinged origin

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ const { Socket } = require('socket.io')
 
 const io=require('socket.io')(8001,{
     cors: {
-        origin: "https://mdsasohail.github.io",
+        // origin: "https://mdsasohail.github.io",
+        // origin:'http://localhost:3000',
+        origin:'*',
         methods: ["GET", "POST"]
       }
 })
@@ -24,17 +26,17 @@ const findSocatId=(receiver)=>{
 io.on("connection",(socket)=>{
     console.log("A user is connected");
     socket.on("addUser",(userId)=>{
-        // console.log("Add the user id "+user) 
+        // console.log("Add the user id "+user)  
         addUser(socket.id,userId);
         io.emit("getUsers",users);
     })
 
 
-
+ 
 
     socket.on('sendMessage',async({senderId,receiverId,text})=>{
         console.log("Receiver id is in Socket "+receiverId);
-           const receiver= await findSocatId(receiverId);
+           const receiver= await findSocatId(receiverId)
            console.log(receiver);
             io.to(receiver?.socket).emit("getMessage",{senderId,text})
     })
